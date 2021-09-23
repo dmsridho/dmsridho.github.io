27648bc1a4cf174a0b19a558bea9a6616cdc0de4
Dimas Ridho Amali <dimasridho69@gmail.com>
2021-09-23 17:55:43 +0800

    Update and rename kalkulator.php to index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kalkulator Lumayan</title>
-    <link rel="stylesheet" href="Tugas4_Dimas Ridho Amali.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 
